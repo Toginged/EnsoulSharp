f8405769ac424164059e4231b26f367b6dc02ed7
Toginged <53311468+Toginged@users.noreply.github.com>
2019-07-25 19:45:08 +0300

    BY FrOnDaL

---
+++ FrOnDaL AIO/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using FrOnDaL_AIO.Champions;
+
+namespace FrOnDaL_AIO
+{
+    internal class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnLoadingComplete;
+        }
+        private static void OnLoadingComplete()
+        {
+            try
+            {
+                switch (GameObjects.Player.CharacterName)
+                {
+                    case "Ashe":
+                        Ashe.GameOn();
+                        Chat.Print("FrOnDaL AIO " + ObjectManager.Player.CharacterName + " Loaded <font color='#1dff00'>by FrOnDaL</font>");
+                        Console.WriteLine("FrOnDaL AIO " + ObjectManager.Player.CharacterName + " loaded");
+                        break;
+                    case "Katarina":
+                        Katarina.GameOn();
+                        Chat.Print("FrOnDaL AIO " + ObjectManager.Player.CharacterName + " Loaded <font color='#1dff00'>by FrOnDaL</font>");
+                        Console.WriteLine("FrOnDaL AIO " + ObjectManager.Player.CharacterName + " loaded");
+                        break;
+                    case "Orianna":
+                        Orianna.GameOn();
+                        Chat.Print("FrOnDaL AIO " + ObjectManager.Player.CharacterName + " Loaded <font color='#1dff00'>by FrOnDaL</font>");
+                        Console.WriteLine("FrOnDaL AIO " + ObjectManager.Player.CharacterName + " loaded");
+                        break;
+                    default:
+                        Chat.Print("<font color='#b756c5' size='25'>FrOnDaL AIO Does Not Support " + ObjectManager.Player.CharacterName + " - By FrOnDaL</font>");
+                        Console.WriteLine("FrOnDaL AIO Does Not Support " + ObjectManager.Player.CharacterName + " - By FrOnDaL");
+                        break;                  
+                }
+            }
+            catch (Exception)
+            {
+                Chat.Print("Error in loading - By FrOnDaL");
+                Console.WriteLine("Error in loading - By FrOnDaL");
+            }
+        }
+    }
+}
